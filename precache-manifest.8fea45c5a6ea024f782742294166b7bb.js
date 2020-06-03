2ad1194968906c5e316bb299cc0d805a9dd0f309
Aneta Jóźwiak <aneta_jozwiak@op.pl>
2020-06-03 13:17:40 +0200

    Updates

---
+++ precache-manifest.8fea45c5a6ea024f782742294166b7bb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bcc3c29033000f786650ba428aeca2",
+    "url": "/portfolio-lab/index.html"
+  },
+  {
+    "revision": "86b6761736d355634162",
+    "url": "/portfolio-lab/static/css/main.9e52ac73.chunk.css"
+  },
+  {
+    "revision": "dd260ad7b49a0cdd6078",
+    "url": "/portfolio-lab/static/js/2.0bb8c28d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-lab/static/js/2.0bb8c28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b6761736d355634162",
+    "url": "/portfolio-lab/static/js/main.fdac51a2.chunk.js"
+  },
+  {
+    "revision": "dbed60aa7057cff87084",
+    "url": "/portfolio-lab/static/js/runtime-main.07616303.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/portfolio-lab/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/portfolio-lab/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/portfolio-lab/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/portfolio-lab/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/portfolio-lab/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/portfolio-lab/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/portfolio-lab/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/portfolio-lab/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/portfolio-lab/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/portfolio-lab/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/portfolio-lab/static/media/Signature.e54cfcfc.svg"
+  }
+]);
